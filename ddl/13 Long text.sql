b61bbaa34c15cece60002fe940123f86a6913ab2
Jari Laine <jariolaine@users.noreply.github.com>
2014-12-11 16:09:34 +0000

    Bug fix and new features

--- ddl/13 Long text.sql
+++
@@ -1,6 +0,0 @@
-REM INSERTING into BLOG_LONG_TEXT
-SET DEFINE OFF;
-Insert into BLOG_LONG_TEXT (LONG_TEXT_TYPE,LONG_TEXT_DESCRIPTION) values ('FOOTER','Footer text shown on every page');
-Insert into BLOG_LONG_TEXT (LONG_TEXT_TYPE,LONG_TEXT_DESCRIPTION) values ('ABOUT','About page text');
-Insert into BLOG_LONG_TEXT (LONG_TEXT_TYPE,LONG_TEXT_DESCRIPTION) values ('CONTACT','Contact page information');
-Insert into BLOG_LONG_TEXT (LONG_TEXT_TYPE,LONG_TEXT_DESCRIPTION) values ('DISCLAIMER','Blog site disclaimer');
