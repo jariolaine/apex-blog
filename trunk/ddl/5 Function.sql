fe8a331d7169c779d90fbc4fe2e68c91e6683c4a
jarola <jarola@187959b7-9153-4e9b-b8e4-706ab9fd519f>
2015-01-01 10:42:00 +0000

    dynamically create blog_pw_hash function salt using dbms_random

--- trunk/ddl/5 Function.sql
+++ trunk/ddl/5 Function.sql
@@ -5,7 +5,9 @@ BEGIN
   RETURN TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMMDDHH24MISS') || LPAD(blog_seq.nextval,4,0));
 END "BLOG_SGID";
 /
-CREATE OR REPLACE FUNCTION  "BLOG_PW_HASH" (
+begin
+execute immediate '
+CREATE OR REPLACE FUNCTION "BLOG_PW_HASH" (
   p_username IN VARCHAR2,
   p_password IN VARCHAR2
 ) RETURN VARCHAR2
@@ -13,13 +15,13 @@ AUTHID DEFINER
 AS
 /* This function should be wrapped, as the hash algorhythm for password is exposed. */
   l_password VARCHAR2(4000);
-  l_salt     VARCHAR2(255) := 'EVQELZY27PVLWPHMRN8B0CRRMAXBR8';
+  l_salt     VARCHAR2(255) := ''' || sys.dbms_random.string('A',40) || ''';
 BEGIN
   l_password  :=
         SUBSTR(l_salt, 8, 13)
     ||  sys.utl_raw.cast_to_raw(p_password)
     ||  SUBSTR(l_salt, 4, 10)
-	||  SUBSTR(l_salt, 22, 28)
+    ||  SUBSTR(l_salt, 22, 28)
     ||  sys.utl_raw.cast_to_raw(p_username)
     ||  SUBSTR(l_salt, 18, 26)
   ;
@@ -27,4 +29,6 @@ BEGIN
   l_password := sys.dbms_obfuscation_toolkit.md5(input_string => l_password);
   RETURN l_password;
 END "BLOG_PW_HASH";
+';
+end;
 /
