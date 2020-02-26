0a50d72f3a8f5a122635656d41f4b56b5b002db8
jariolaine <jariolaine@users.noreply.github.com>
2020-02-26 21:19:59 +0200

    Type export

--- Database/Types/BLOG_T_TAG_ID.sql
+++ Database/Types/BLOG_T_TAG_ID.sql
@@ -1,14 +1,9 @@
 --------------------------------------------------------
---  File created - Friday-January-03-2020
---------------------------------------------------------
---------------------------------------------------------
 --  DDL for Type BLOG_T_TAG_ID
 --------------------------------------------------------
 
-CREATE OR REPLACE TYPE "BLOG_T_TAG_ID"
-authid definer
-as
-object (
-  tag_id number(38,0)
-)
+  CREATE OR REPLACE TYPE "BLOG_T_TAG_ID" 
+as 
+object (tag_id number(38,0))
+
 /
---
+++ Database/Types/Generated-20200226211905.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  File created - Wednesday-February-26-2020   
+--------------------------------------------------------
+@/home/lainfjar/BLOG_T_TAG_ID.sql
+@/home/lainfjar/BLOG_T_TAG_IDS.sql
