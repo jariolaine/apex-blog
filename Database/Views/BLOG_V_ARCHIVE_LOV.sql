f092523eef61bb1633ec4785f85c74cf81f709c3
jariolaine <jariolaine@users.noreply.github.com>
2020-03-28 08:06:04 +0200

    Added column published_on

--- Database/Views/BLOG_V_ARCHIVE_LOV.sql
+++ Database/Views/BLOG_V_ARCHIVE_LOV.sql
@@ -2,11 +2,13 @@
 --  DDL for View BLOG_V_ARCHIVE_LOV
 --------------------------------------------------------
 
-  CREATE OR REPLACE FORCE VIEW "BLOG_V_ARCHIVE_LOV" ("YEAR_MONTH", "POSTS_COUNT") AS 
-  select t1.year_month
-  ,count( t1.post_id ) as posts_count
+CREATE OR REPLACE FORCE VIEW "BLOG_V_ARCHIVE_LOV" ("YEAR_MONTH", "PUBLISHED_ON", "POSTS_COUNT") AS
+select t1.year_month              as year_month
+  ,trunc( t1.published_on, 'MM' ) as published_on
+  ,count( t1.post_id )            as posts_count
 from blog_v_posts t1
 where 1 = 1
 group by t1.year_month
+  ,trunc( t1.published_on, 'MM' )
 with read only
 ;
